0637262d78f6130ce90c92e0b24bdc7c00a32806
Gabriel Kabbe <gabriel.kabbe@mail.de>
2022-10-20 22:59:04 +0200

    Add struct macro

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+#![feature(trace_macros)]
+#![feature(log_syntax)]
+
 macro_rules! times_5 {
     ($e:expr) => {
         5 * $e
@@ -16,8 +19,21 @@ macro_rules! mul {
     };
 }
 
+macro_rules! make_struct {
+    ($id:ident) => {
+        #[derive(Debug)]
+        struct $id(i32);
+    };
+}
+
+trace_macros!(true);
+make_struct!(Blablub);
+make_struct!(Blablib);
+trace_macros!(false);
+
 fn main() {
     println!("{}", times_5!(123));
     println!("{}", mul![1, 2, 3, 4, 5]);
     println!("{}", mul![]);
+    println!("{:?}", Blablib(123));
 }
