25e6c04a3016373af71e8b2210d955ad7b1d982d
Gabriel Kabbe <gabriel.kabbe@mail.de>
2022-10-21 22:45:25 +0200

    Generic parsing works for i32

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
 #![feature(trace_macros)]
 #![feature(log_syntax)]
 
+use nom::bytes::complete::tag;
+use nom::character::complete::char;
+use nom::character::complete::digit1;
+use nom::combinator::map;
+use nom::combinator::map_res;
+use nom::sequence::preceded;
+use nom::sequence::tuple;
+use nom::IResult;
+use std::{io, str::FromStr};
+
 macro_rules! times_5 {
     ($e:expr) => {
         5 * $e
@@ -20,15 +30,28 @@ macro_rules! mul {
 }
 
 macro_rules! make_struct {
-    ($id:ident, $type:ty) => {
+    ($id:ident) => {
         #[derive(Debug)]
-        struct $id($type);
+        struct $id<T: FromStr>(T);
+        impl<T> $id<T>
+        where
+            T: FromStr,
+        {
+            fn parse(input: &str) -> IResult<&str, Self> {
+                let prefix = tuple((tag("byr"), char(':')));
+                let parse_numbers = map_res(digit1, T::from_str);
+                let parser = preceded(prefix, parse_numbers);
+                let mut p = map(parser, Self);
+                p(input)
+            }
+        }
     };
 }
 
 trace_macros!(true);
-make_struct!(Blablub, i32);
-make_struct!(Blablib, u32);
+make_struct!(Blablub);
+make_struct!(Blablib);
+make_struct!(ABC);
 trace_macros!(false);
 
 fn main() {
@@ -36,5 +59,5 @@ fn main() {
     println!("{}", mul![1, 2, 3, 4, 5]);
     println!("{}", mul![]);
     println!("{:?}", Blablub(-123));
-    println!("{:?}", Blablib(123));
+    println!("{:?}", Blablib::<i32>::parse("byr:499"));
 }
