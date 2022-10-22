99e6a7a5cb73986a392d538e76ef4f71da22df96
Gabriel Kabbe <gabriel.kabbe@mail.de>
2022-10-22 14:15:47 +0200

    Implement parse for &str

--- src/main.rs
+++ src/main.rs
@@ -35,42 +35,42 @@ trait Parse {
 }
 
 macro_rules! make_struct {
-    ($id:ident, $tag:literal) => {
+    ($id:ident, &str, $tag:literal) => {
         #[derive(Debug)]
-        struct $id<T: FromStr>(T);
-        impl<T> Parse for $id<T>
-        where
-            T: FromStr,
-        {
+        struct $id<'a>(&'a str);
+        impl<'a> $id<'a> {
+            fn parse(input: &'a str) -> IResult<&'a str, Self> {
+                let prefix = tuple((tag($tag), char(':')));
+                let parser = preceded(prefix, alpha1);
+                let mut p = map(parser, Self);
+                p(input)
+            }
+        }
+    };
+    ($id:ident,$type:ty, $tag:literal) => {
+        #[derive(Debug)]
+        struct $id($type);
+        impl $id {
             fn parse(input: &str) -> IResult<&str, Self> {
                 let prefix = tuple((tag($tag), char(':')));
-                let parse_numbers = map_res(digit1, T::from_str);
+                let parse_numbers = map_res(digit1, <$type>::from_str);
                 let parser = preceded(prefix, parse_numbers);
                 let mut p = map(parser, Self);
                 p(input)
             }
         }
-
-        // impl Parse for $id<&str> {
-        //     fn parse(input: &str) -> IResult<&str, Self> {
-        //         let prefix = tuple((tag($tag), char(':')));
-        //         let parser = preceded(prefix, alpha1);
-        //         let mut p = map(parser, Self);
-        //         p(input)
-        //     }
-        // }
     };
 }
 
 trace_macros!(true);
-make_struct!(BirthYear, "byr");
-make_struct!(EyeColor, "ecl");
+make_struct!(BirthYear, u32, "byr");
+make_struct!(EyeColor, &str, "ecl");
 trace_macros!(false);
 
 fn main() {
     println!("{}", times_5!(123));
     println!("{}", mul![1, 2, 3, 4, 5]);
     println!("{}", mul![]);
-    println!("{:?}", BirthYear::<i32>::parse("byr:499"));
-    // println!("{:?}", EyeColor::<&str>::parse("byr:499"));
+    println!("{:?}", BirthYear::parse("byr:499"));
+    println!("{:?}", EyeColor::parse("ecl:blablub"));
 }
